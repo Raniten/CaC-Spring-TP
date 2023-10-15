835bf4c5bc0b696028e6ae67c5e116571c167a6c
Marcelo Ranzani <raniten@gmail.com>
2023-10-15 10:18:57 -0300

    Reinicio
    Reinicio

---
+++ src/main/java/com/cac0/g6/tpfinal/TpfinalApplication.java
@@ -0,0 +1,13 @@
+package com.cac0.g6.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cac0/g6/tpfinal/TpfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cac0.g6.tpfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
